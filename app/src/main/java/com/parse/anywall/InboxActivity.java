6ed41a23bdc7abeaf4adcf6bc3d3d9c95fc7a9c8
Rohit Dubey <rohitdubey12@gmail.com>
2015-07-03 23:46:54 -0400

    first commit

--- app/src/main/java/com/parse/anywall/InboxActivity.java
+++ app/src/main/java/com/parse/anywall/InboxActivity.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class SignUpActivity extends Activity {
+public class InboxActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_inbox);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
+        getMenuInflater().inflate(R.menu.menu_inbox, menu);
         return true;
     }
 
